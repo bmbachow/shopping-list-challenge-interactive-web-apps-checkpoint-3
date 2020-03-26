cf26a0a2fe2ed7922d244a47ffe097e7cfb5bf13
Brian Bachow <bmbachow@gmail.com>
2020-03-25 23:40:48 -0700

    removed modified css

---
+++ index.js
@@ -0,0 +1,14 @@
+// write a function that when click on check button, the text above gets crossed out.  use toggle method so that when clicked again the text gets uncrossed
+
+// write a function that when click the delete button, the container box gets removed
+
+// write a function that takes the value inputted in the form field and creates a new box below 
+
+
+function submitHandler ($('#js-shopping-list-form button').submit(event=> {
+  event.preventDefault()
+  console.log('handler for .submit() called')
+  ($.shopping-list).append(#shopping-list-entry.text)
+
+
+ // toggle .shopping-item__checked
