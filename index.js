d2c661039a8aef855bd34f5902e6bcc24ac2cda7
Brian Bachow <bmbachow@gmail.com>
2020-03-26 18:03:10 -0700

    finished toggle checked and delete

--- index.js
+++ index.js
@@ -4,13 +4,35 @@
 
 // write a function that takes the value inputted in the form field and creates a new box below 
 
- 
-function submitHandler $('#js-shopping-list-form button').submit(event=> {
-  event.preventDefault()
-  console.log('handler for .submit() called')
-  ($.shopping-list).append(li))
+
+function handleShoppingList(){
+
+  $('#js-shopping-list-form').submit(event=> {
+    event.preventDefault();
+    console.log('handler for .submit() called');
+  
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(e){
+    e.preventDefault();
+    console.log('handler for shopping item toggle');
+    //on whatever I just clicked go to the parent, go to the parent of that, go to the parent above, find the shopping item and add the class shopping-item-toggle
+    $(this).parent().parent().find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+  
+
+  $('.shopping-list').on('click', '.shopping-item-delete', function(e){
+    e.preventDefault();
+    console.log('handler for delete item');
+    $(this).parent().parent().remove();
+  });
+  
 
 }
+
+
+$(handleShoppingList);
+
 
 //. variable whose value is set to a function that creates a new block containing (#shopping-list-entry.text)
 
